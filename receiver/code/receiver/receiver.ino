1ba65b31d2625c3c81d50e3fdc941e84c2d7b280
sami0-2 <samsonamanuel6992@gmail.com>
2023-11-16 21:17:48 +0300

    Added the receiver codes both have not been tested
    Completed the receiver codes, however the receiver will not do anything with the received data it just stores it and test codes have not been treid yet

--- receiver/code/receiver/receiver.ino
+++ receiver/code/receiver/receiver.ino
@@ -1,9 +1,43 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8);
+const byte address[6] = "add21";
+
+
+//Values to be received
+struct DATA_PACKAGE
+{
+  int analogVal[6] = {};
+  bool digitalVal[6] = {};
+};
+DATA_PACKAGE data;
+
+
 void setup()
 {
+  //Initializing the DATA_PACKAGE
+  for(int i = 0; i < 6; i++)
+  {
+    data.analogVal[i] = 127;
+    data.digitalVal[i] = true;
+  }
+
+
   
+  //Initalizing the radio communication 
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
 }
 
 void loop()
 {
+  if(radio.available())
+  {
+    radio.read(&data, sizeof(DATA_PACKAGE));
+  }
   
 }
